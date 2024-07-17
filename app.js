948f6c88c86fab849eeaec80c40457fb047b715c
Obua Denis Daniel <dd.obua>
2024-07-17 17:39:20 +0300

    Refactor tour operations

--- app.js
+++ app.js
@@ -3,32 +3,28 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// Tours resource
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours } });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
-
   if (!tour) {
     return res.status(404).json({ status: 'fail', message: '  Invalid ID' });
   }
-
   res.status(200).json({ status: 'success', data: { tour } });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -41,29 +37,33 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({ status: 'success', data: { tour: newTour } });
     }
   );
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   const id = req.params.id * 1;
-
   if (id > tours.length) {
     return res.status(404).json({ status: 'fail', message: '  Invalid ID' });
   }
-
   res
     .status(200)
     .json({ status: 'success', data: { tour: '<Updated tour here...>' } });
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   const id = req.params.id * 1;
-
   if (id > tours.length) {
     return res.status(404).json({ status: 'fail', message: '  Invalid ID' });
   }
-
   res.status(204).json({ status: 'success', data: null });
-});
+};
+
+// Tours resource
+app.get('/api/v1/tours', getAllTours);
+app.post('/api/v1/tours', createTour);
+
+app.get('/api/v1/tours/:id', getTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
 
 // Start server
 const port = 3000;
