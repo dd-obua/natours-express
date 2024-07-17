97bc4f9409afe8744aebdcf11e9eb7091d0b4082
Obua Denis Daniel <dd.obua>
2024-07-17 13:50:14 +0300

    Handle get request by sending back tours in JSend format

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// Tours resource
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours } });
+});
+
 // Start server
 const port = 3000;
 app.listen(port, '127.0.0.1', () => {
