50e6fe650ef669949db6235684dc9034efb40568
Obua Denis Daniel <dd.obua>
2024-07-17 16:18:35 +0300

    Respond to request of a paticular tour information using tour id

--- app.js
+++ app.js
@@ -17,9 +17,14 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
-app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
 
+  res.status(200).json({ status: 'success', data: { tour } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
