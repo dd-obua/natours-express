c25732aeeabcfcb8968947229dac1c60b664aa12
Obua Denis Daniel <dd.obua>
2024-07-17 15:06:09 +0300

    Handle post request by adding a new tour

--- app.js
+++ app.js
@@ -17,6 +17,23 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  // Update tours send response
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
+});
+
 // Start server
 const port = 3000;
 app.listen(port, '127.0.0.1', () => {
