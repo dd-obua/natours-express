f737dbb41a7fa0621316c9a3782954a7ea2746f1
Obua Denis Daniel <dd.obua>
2024-07-17 17:24:02 +0300

    Handle deletions based on specific ids

--- app.js
+++ app.js
@@ -55,6 +55,16 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     .json({ status: 'success', data: { tour: '<Updated tour here...>' } });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: '  Invalid ID' });
+  }
+
+  res.status(204).json({ status: 'success', data: null });
+});
+
 // Start server
 const port = 3000;
 app.listen(port, '127.0.0.1', () => {
