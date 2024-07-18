bd52b7ea77d332555b4519754da9c06598c1a40c
Obua Denis Daniel <dd.obua>
2024-07-18 18:12:37 +0300

    Configure and use environment variables

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const dovenv = require('dotenv');
 const app = require('./app');
 
+dovenv.config({ path: '/config.env' });
+
 // Start server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
   console.log();
   console.log(`App running on port ${port}...`);
