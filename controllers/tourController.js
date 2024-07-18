1bee34efab7ca3cc9701d56840a1ff3f6c09ee7a
Obua Denis Daniel <dd.obua>
2024-07-18 17:11:53 +0300

    Use checkID middleware to validate request ids

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,9 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-exports.checkID = (id, req, res, next) => {
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is ${val}`);
+
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({ status: 'fail', message: 'Invalid id' });
   }
@@ -26,9 +28,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: '  Invalid ID' });
-  }
   res.status(200).json({ status: 'success', data: { tour } });
 };
 
@@ -48,19 +47,11 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
-  if (id > tours.length) {
-    return res.status(404).json({ status: 'fail', message: '  Invalid ID' });
-  }
   res
     .status(200)
     .json({ status: 'success', data: { tour: '<Updated tour here...>' } });
 };
 
 exports.deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-  if (id > tours.length) {
-    return res.status(404).json({ status: 'fail', message: '  Invalid ID' });
-  }
   res.status(204).json({ status: 'success', data: null });
 };
