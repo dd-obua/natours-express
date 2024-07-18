699293ac6ed01b3ebad2999618fad01b33efb132
Obua Denis Daniel <dd.obua>
2024-07-18 16:59:22 +0300

    Define checkID middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,13 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (id, req, res, next) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
+  }
+  next();
+};
+
 // Route handlers
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
