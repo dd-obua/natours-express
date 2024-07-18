3731e47e99c60641c24acc106e1d2d371f9ae6ac
Obua Denis Daniel <dd.obua>
2024-07-18 13:34:29 +0300

    Refactor controllers into seperate files

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,37 +1,14 @@
 const express = require('express');
 
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require('./../controllers/userController');
+
 const router = express.Router();
-
-// Route handlers
-const getAllUsers = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defined' });
-};
-
-const createUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defined' });
-};
-
-const getUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defined' });
-};
-
-const updateUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defined' });
-};
-
-const deleteUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defined' });
-};
 
 router.route('/api/v1/users').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
