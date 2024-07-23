b7ffc1b68fb1d9b81ae29f8d1ef0dd993d909308
Obua Denis Daniel <dd.obua>
2024-07-23 10:07:15 +0300

    Connect to the remote database

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
+mongoose
+  .connect(DB)
+  .then(() => console.log(`Database connection successful`))
+  .catch((error) => console.error(`Error: ${error}`));
+
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
