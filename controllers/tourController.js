e89fde9087df8f1c4f4de5dba6a0eb96530563e8
Obua Denis Daniel <dd.obua>
2024-07-23 11:39:17 +0300

    Clean up code in tourController and tourRoute files

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,4 @@
-const fs = require('fs');
 const Tour = require('../model/tourModel');
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is ${val}`);
-
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
-  }
-  next();
-};
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -25,31 +15,26 @@ exports.getAllTours = (req, res) => {
 
   res.status(200).json({
     status: 'success',
-    results: tours.length,
-    requestedAt: req.requestTime,
-    data: { tours },
+    // results: tours.length,
+    // requestedAt: req.requestTime,
+    // data: { tours },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({ status: 'success', data: { tour } });
+  // const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({ status: 'success', data: { tour } });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
   // Update tours send response
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (error) => {
-      res.status(201).json({ status: 'success', data: { tour: newTour } });
-    },
-  );
+  res.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 exports.updateTour = (req, res) => {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,13 +6,11 @@ const {
   createTour,
   updateTour,
   deleteTour,
-  checkID,
   checkBody,
-} = require('./../controllers/tourController');
+} = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', checkID);
 router.route('/').get(getAllTours).post(checkBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
