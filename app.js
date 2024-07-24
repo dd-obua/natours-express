10b3c01e1e9421553190baa70d3cc7a27087e404
Obua Denis Daniel <dd.obua>
2024-07-24 09:22:51 +0300

    Deactive middlware

--- app.js
+++ app.js
@@ -13,16 +13,15 @@ if (process.env.NODE_ENV === 'developmet') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-// Middleware
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
+// // Middleware
+// app.use((req, res, next) => {
+//   next();
+// });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
 // Mounting routers
 app.use('/api/v1/tours', tourRouter); // middleware and sub application
