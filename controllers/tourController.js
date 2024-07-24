a7c5ecd58b6a1d52b9f2bb38989d457fb57148b9
Obua Denis Daniel <dd.obua>
2024-07-24 14:56:06 +0300

    Remove page, sort, limit and fields from query string if found

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,11 @@ const Tour = require('../model/tourModel');
 // Route handlers
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find(req.query);
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const tours = await Tour.find(queryObj);
 
     res.status(200).json({
       status: 'success',
