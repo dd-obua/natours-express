afccfd59d57b89d02ff31210b116577e2d5f5ffb
Obua Denis Daniel <dd.obua>
2024-07-24 08:41:48 +0300

    Create tour deletion functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const Tour = require('../model/tourModel');
 
 // Route handlers
@@ -53,6 +54,11 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({ status: 'success', data: null });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({ status: 'success', data: null });
+  } catch (error) {
+    res.status(404).json({ status: 'fail', message: error });
+  }
 };
