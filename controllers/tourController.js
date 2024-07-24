6541a619a8a53b05faeae2df4d99f836219a4681
Obua Denis Daniel <dd.obua>
2024-07-24 13:27:07 +0300

    Enable search using qurey string

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,7 @@ const Tour = require('../model/tourModel');
 // Route handlers
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const tours = await Tour.find(req.query);
 
     res.status(200).json({
       status: 'success',
